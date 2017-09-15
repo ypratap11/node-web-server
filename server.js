823796c66a1f1bbdfd91275666bc34eef27aa8dd
Pratap Yeragudipati <ypratap114u@gmail.com>
2017-09-14 23:04:53 -0700

    Setup start script and heroku part

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 //hbs.registerPartials(__dirname + './views/partials')
@@ -38,6 +40,6 @@ app.get('/bad',(req,res) => {
   });
 });
 
-app.listen(3000,() => {
-  console.log('Sever Started on port 3000');
+app.listen(port,() => {
+  console.log(`Sever Started on port ${port}`);
 });
